b9041e568dcad9fc3d387407ccf082162fb5e9b8
wetsoggydoggy7 <66643026+wetsoggydoggy7@users.noreply.github.com>
2024-02-08 14:02:13 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ static/assets/js/games.js
@@ -0,0 +1,124 @@
+import { createViewPage, isValidURL, PolarisError } from './utils.js';
+import effects from './effects.js';
+
+const load = () => {
+    fetch('/api/games')
+        .then(res => res.json())
+        .then(games => {
+            const searchBar = document.querySelector('#searchInput');
+
+            searchBar.setAttribute('placeholder', `Search ${games.all.length} Games`);
+
+            searchBar.addEventListener('input', () => {
+                console.log(searchBar.value);
+
+                if (searchBar.value) {
+                    var result = false;
+
+                    document.querySelectorAll('.games>.game').forEach(game => {
+                        if (game.querySelector('h3').textContent.toLowerCase().includes(searchBar.value.toLowerCase())) {
+                            result = true;
+
+                            game.classList.remove('hidden');
+                        }
+                        else game.classList.add('hidden');
+                    });
+
+                    if (result) document.querySelector('.searchErr').classList.add('hidden');
+                    else document.querySelector('.searchErr').classList.remove('hidden');
+                } else {
+                    document.querySelectorAll('.game').forEach(game => game.classList.remove('hidden'));
+                    document.querySelector('.searchErr').classList.add('hidden');
+                }
+            });
+
+            games.popular.forEach(game => {
+                const popularEl = document.createElement('div');
+                popularEl.classList = 'game';
+                document.querySelector('.popular-games').appendChild(popularEl);
+                
+                const image = document.createElement('img');
+                image.src = game.image;
+                image.loading = 'lazy';
+                image.onerror = () => image.src = '/assets/img/logo.png';
+                popularEl.appendChild(image);
+
+                const name = document.createElement('h3');
+                name.textContent = game.name;
+                popularEl.appendChild(name);
+
+                popularEl.addEventListener('click', async () => {
+                    document.body.style.opacity = '0.7';
+
+                    umami.track('popular-game_' + game.name);
+
+                    setTimeout(() => {
+                        if (isValidURL(game.target)) createViewPage({
+                            target: game.target,
+                            title: game.name,
+                            proxied: true
+                        });
+                        else createViewPage({
+                            target: game.target,
+                            title: game.name
+                        });
+                    }, 500);
+                });
+
+                effects.hoverTilt({
+                    max: 8,
+                    perspective: 1000,
+                    scale: 1.05,
+                    speed: 800,
+                    easing: 'cubic-bezier(.03,.98,.52,.99)'
+                }, popularEl);
+            });
+
+            games.all.forEach(game => {
+                const el = document.createElement('div');
+                el.classList = 'game';
+                document.querySelector('.games').appendChild(el);
+
+                const image = document.createElement('img');
+                image.src = game.image;
+                image.loading = 'lazy';
+                image.onerror = () => image.src = '/assets/img/logo.png';
+                el.appendChild(image);
+
+                const name = document.createElement('h3');
+                name.textContent = game.name;
+                el.appendChild(name);
+
+                effects.hoverTilt({
+                    max: 8,
+                    perspective: 1000,
+                    scale: 1.05,
+                    speed: 800,
+                    easing: 'cubic-bezier(.03,.98,.52,.99)'
+                }, el);
+
+                el.addEventListener('click', async () => {
+                    document.body.style.opacity = '0.7';
+
+                    umami.track('game-' + game.name);
+
+                    setTimeout(() => {
+                        if (isValidURL(game.target)) createViewPage({
+                            target: game.target,
+                            title: game.name,
+                            proxied: true
+                        });
+                        else createViewPage({
+                            target: game.target,
+                            title: game.name
+                        });
+                    }, 1000);
+                });
+            });
+        })
+        .catch(e => new PolarisError('Failed to load games'));
+};
+
+export default {
+    load
+};
---
+++ static/assets/js/view.js
@@ -0,0 +1,78 @@
+import { loadProxyWorker, encoder, storage } from './utils.js';
+import { loadSettings } from './settings.js';
+
+loadSettings();
+
+const params = new URLSearchParams(location.search);
+const settingsStorage = storage('settings');
+
+window.history.replaceState({}, '', location.pathname);
+
+if (params.get('load')) {
+    try {
+        const parsedData = JSON.parse(atob(params.get('load')));
+
+        if (Boolean(parsedData.target && parsedData.title && parsedData.return)) {
+            document.body.classList.remove('hidden');
+
+            sessionStorage.setItem('loaddata', JSON.stringify(parsedData));
+
+            if (parsedData.proxied) {
+                await loadProxyWorker(settingsStorage.get('proxy') || 'uv');
+
+                document.querySelector('#loadframe').src = `/${settingsStorage.get('proxy') || 'uv'}/service/${encoder['xor'].encode(parsedData.target)}`;
+            } else document.querySelector('#loadframe').src = parsedData.target;
+
+            document.querySelector('#loadframe').addEventListener('load', () => {
+                document.querySelector('.title').textContent = parsedData.title;
+
+                document.querySelector('#loadframe').style.transition = 'none';
+                document.querySelector('#loadframe').style.background = '#fff';
+                
+                document.querySelector('#loadframe').addEventListener('mouseover', () => {
+                    document.querySelector('.gamebar').classList.add('collapsed');
+                    document.querySelector('.hitbox').classList.remove('active');
+                });
+
+                document.querySelector('#loadframe').addEventListener('mouseout', () => {
+                    document.querySelector('.gamebar').classList.remove('collapsed');
+                    document.querySelector('.hitbox').classList.add('active');
+                });
+
+                setTimeout(() => {
+                    document.querySelector('.gamebar').classList.remove('collapsed');
+                    document.querySelector('.hitbox').classList.add('active');
+                }, 1000);
+            });
+
+            document.querySelector('#fullscreen').addEventListener('click', () => {
+                const frame = document.querySelector('#loadframe');
+
+                if (frame.requestFullscreen) frame.requestFullscreen();
+                else if (frame.webkitRequestFullscreen) frame.webkitRequestFullscreen();
+                else if (frame.mozRequestFullScreen) frame.mozRequestFullScreen();
+                else if (frame.msRequestFullscreen) frame.msRequestFullscreen();
+            });
+
+            window.addEventListener('fullscreenchange', () => {
+                if (document.fullscreenElement) document.querySelector('#loadframe').style.borderRadius = '0px';
+                else document.querySelector('#loadframe').style.borderRadius = '';
+            });
+
+            document.querySelector('#return').addEventListener('click', () => {
+                document.body.style.opacity = '0.7';
+                
+                setTimeout(() => window.location.href = parsedData.return, 500);
+            });
+        } else if (parsedData.target && parsedData.redirect === true) {
+            window.history.replaceState({}, '', '/redirect');
+
+            if (parsedData.trusted) window.location.replace(parsedData.target);
+            else {
+                document.documentElement.textContent = `Redirecting to ${parsedData.target}`;
+                setTimeout(() => window.location.replace(parsedData.target), 1000);
+            }
+        } else window.location.replace(parsedData.return || '/');
+    } catch (e) { alert(e); window.location.replace('/'); }
+} else if (sessionStorage.getItem('loaddata')) window.location.replace(`/view?load=${btoa(sessionStorage.getItem('loaddata'))}`);
+else window.location.replace('/');
